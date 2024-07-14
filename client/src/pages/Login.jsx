c270e29ff1d73c2cff1bfd8aa3ebba79eed42f18
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-07-14 06:13:01 +0530

    folder setup

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      hi  i am your login
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/pages/TaskDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TaskDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TaskDetails
